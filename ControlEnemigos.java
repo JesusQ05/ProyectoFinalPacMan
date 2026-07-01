af99df62f358d186941654b4e4a0c6f0265fdf79
JesusQ05 <jesusquirozcasquino@gmail.com>
2026-07-01 10:54:34 -0500

    ControlEnemigos

---
+++ ControlEnemigos.java
@@ -0,0 +1,26 @@
+public class ControlEnemigos {
+
+    private Enemigo[] enemigos;
+    private Jugador jugador;
+
+    public ControlEnemigos(Enemigo[] enemigos, Jugador jugador) {
+        this.enemigos = enemigos;
+        this.jugador = jugador;
+    }
+
+    public void moverEnemigos() {
+        
+        }
+        
+        public void verificarColisiones() {
+        
+        }
+
+        public void eliminarEnemigosInactivos() {
+
+        }
+
+        public void generarMovimientos() {
+
+        }
+    }
